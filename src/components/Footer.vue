d6d28ede60ef37c69214c5d1911ade4d12ecae4f
soukphasone <soukphasone2001@gmail.com>
2024-12-18 11:14:51 +0700

    test

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer v-if="hiddenContent">
-    <div class="container mx-auto py-0 px-0">
+    <div class="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl mx-">
       <img src="@/assets/img/Direct-banking-lvb_04.jpg" alt="logo" />
     </div>
   </footer>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
      <header class="bg-gray-100">
-      <div class="container mx-auto py-0 px-0">
+      <div class="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl mx-auto">
         <img
           src="/src/assets/img/Direct-banking-lvb_01.jpg"
           alt="logo"
-          class="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-xl xl:max-w-screen-2xl mx-auto"
+          class=""
         />
       </div>
     </header>
